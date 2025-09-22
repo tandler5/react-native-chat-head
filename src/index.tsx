fa9e049a69b0ff5ef901969dc577527670a3b62f
Marek Tandler <76787781+tandler5@users.noreply.github.com>
2025-09-22 17:50:05 +0200

    fix types

--- src/index.tsx
+++ src/index.tsx
@@ -23,24 +23,24 @@ const ChatHead = isAndroid
   : null;
 
 export function showChatHead(): Promise<boolean> {
-  return isAndroid ? ChatHead.showChatHead() : !!_logWarning();
+  return isAndroid ? ChatHead.showChatHead() : (_logWarning(), false);
 }
 export function hideChatHead(): Promise<boolean> {
-  return isAndroid ? ChatHead.hideChatHead() : !!_logWarning();
+  return isAndroid ? ChatHead.hideChatHead() : (_logWarning(), false);
 }
 export function updateChatBadgeCount(count: number): Promise<boolean> {
   if (typeof count !== 'number') {
     throw new Error('count must be a number');
   }
-  return isAndroid ? ChatHead.updateBadgeCount(count) : !!_logWarning();
+  return isAndroid ? ChatHead.updateBadgeCount(count) : (_logWarning(), false);
 }
 
 export function requrestPermission(): Promise<boolean> {
-  return isAndroid ? ChatHead.requrestPermission() : !!_logWarning();
+  return isAndroid ? ChatHead.requrestPermission() : (_logWarning(), false);
 }
 
 export function checkOverlayPermission(): Promise<boolean> {
-  return isAndroid ? ChatHead.checkOverlayPermission() : !!_logWarning();
+  return isAndroid ? ChatHead.checkOverlayPermission() : (_logWarning(), false);
 }
 const chatHead = {
   showChatHead,
